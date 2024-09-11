9a049a98cdde9876852d5cd361294968822e96ce
Hassam-Ata <hassamata86@gmail.com>
2024-09-12 00:26:23 +0500

    navbar added

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About Us</h1>
+      <p>Learn more about our company and our mission to provide quality trucks for export.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to Truck Export</h1>
+      <p>Find the best trucks for your export needs.</p>
+    </div>
+  );
+}
+
+export default Home;
